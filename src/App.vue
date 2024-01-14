7f2941696cd7947d7a3246f5c35b56827e04ee6b
Ian Jamieson <i@njamieson.co.uk>
2024-01-14 22:58:31 +0000

    update layout

--- src/App.vue
+++ src/App.vue
@@ -30,43 +30,43 @@ const navbarOpen = false;
         </header>
         <img class="object-cover w-full h-full object-right" src="./assets/image/we-dance.jpg" alt="Ian Jamieson stood on a dock with his guitar">
     </section>
-    <section class="p-12 space-y-4">
-        <h2
-            class="font-bold text-gray-600 text-2xl md:text-4xl text-center"
-        >
-            Ian Jamieson on Spotify
-        </h2>
-        <iframe
-            class="rounded-md"
-            src="https://open.spotify.com/embed/artist/2q2E2GheKlqjEBXPUEAcNR?utm_source=generator"
-            width="100%" height="352"
-            frameBorder="0" allowfullscreen="false"
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    </section>
-
-    <section class="p-12">
-        <p
-            class="font-semibold text-gray-800 text-2xl md:text-3xl xl:text-5xl text-center leading-tight"
-        >
-        Music for Ian is more than a passion; it's a heartfelt journey of expression and creativity.
-        </p>
-    </section>
-    <section class="p-12">
-        <img src="./assets/image/ian-jamieson.jpg" />
-    </section>
-
-    <section class="p-12">
-        <p
-            class="font-semibold text-gray-800 text-2xl md:text-3xl xl:text-5xl text-center leading-tight"
-        >
-            Ian's songs are deeply personal, blending melancholic acoustic guitar melodies with lyrics that come straight from the heart.
-        </p>
-    </section>
-    <section class="p-12">
-        <div class="relative w-full h-0 pb-[56.25%]">
-            <iframe class="absolute top-0 left-0 w-full h-full" src="https://www.youtube-nocookie.com/embed/bzviAANSR6c?si=mfLCdTQLNmzOT3AB" title="Ian Jamieson - We Dance YouTube player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </div>
-    </section>
+    <div class="max-w-3xl mx-auto">
+        <section class="p-12 space-y-4">
+            <h2
+                class="font-bold text-gray-600 text-2xl text-center"
+            >
+                Ian Jamieson on Spotify
+            </h2>
+            <iframe
+                class="rounded-md"
+                src="https://open.spotify.com/embed/artist/2q2E2GheKlqjEBXPUEAcNR?utm_source=generator"
+                width="100%" height="352"
+                frameBorder="0" allowfullscreen="false"
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </section>
+        <section class="p-12">
+            <p
+                class="font-semibold text-gray-800 text-2xl text-center leading-tight"
+            >
+            Music for Ian is more than a passion; it's a heartfelt journey of expression and creativity.
+            </p>
+        </section>
+        <section class="p-12">
+            <img src="./assets/image/ian-jamieson.jpg" />
+        </section>
+        <section class="p-12">
+            <p
+                class="font-semibold text-gray-800 text-2xl text-center leading-tight"
+            >
+                Ian's songs are deeply personal, blending melancholic acoustic guitar melodies with lyrics that come straight from the heart.
+            </p>
+        </section>
+        <section class="p-12">
+            <div class="relative w-full h-0 pb-[56.25%]">
+                <iframe class="absolute top-0 left-0 w-full h-full" src="https://www.youtube-nocookie.com/embed/bzviAANSR6c?si=mfLCdTQLNmzOT3AB" title="Ian Jamieson - We Dance YouTube player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+        </section>
+    </div>
 
     <footer>
 
